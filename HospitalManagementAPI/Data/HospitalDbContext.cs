d1345708f75db82244865cc95d923487e5adf112
Ashot Davitavyan <ashotd03@gmail.com>
2024-09-12 17:38:45 +0400

    Defined relationships

--- HospitalManagementAPI/Data/HospitalDbContext.cs
+++ HospitalManagementAPI/Data/HospitalDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using HospitalManagementAPI.Models;
 
 namespace HospitalManagementAPI.Data;
 
@@ -18,6 +19,29 @@ public class HospitalDbContext : DbContext
 	{
 		modelBuilder.Entity<Doctor>()
 			.HasOne<Cabinet>()
+			.WithMany(c => c.Doctors)
+			.HasForeignKey(d => d.CabinetID)
+			.OnDelete(DeleteBehavior.Restrict);
+		
+		modelBuilder.Entity<Doctor>()
+			.HasOne<Specialization>()
+			.WithMany(s => s.Doctors)
+			.HasForeignKey(d => d.SpecializationID)
+			.OnDelete(DeleteBehavior.Restrict);
+
+		modelBuilder.Entity<Doctor>()
+			.HasOne<Region>()
+			.WithMany(r => r.Doctors)
+			.HasForeignKey(d => d.RegionID)
+			.OnDelete(DeleteBehavior.Restrict);
+
+		modelBuilder.Entity<Patient>()
+			.HasOne<Region>()
+			.WithMany(r => r.Patients)
+			.HasForeignKey(p => p.RegionID)
+			.OnDelete(DeleteBehavior.Restrict);
+		
+		
 		base.OnModelCreating(modelBuilder);
 	}
 }
